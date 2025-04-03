ac655be7ba75cd9ac4712542413f15b4f3f03f7d
Jonas G <jp08gim@gmail.com>
2025-04-03 14:45:43 +0200

    patching_bugs

--- FrontEnd/src/components/Game.vue
+++ FrontEnd/src/components/Game.vue
@@ -1,17 +1,28 @@
 <script setup>
+import { onBeforeMount } from 'vue';
 import { useCounterStore } from '../stores/counter';
 import TicTacToe from './TicTacToe.vue';
 
 const useStore = useCounterStore();
+
+const logOut = () => {
+    localStorage.clear();
+    useStore.logOutUser();
+}
+
 </script>
 
 <template>
     <header>
         <h3> Here is your Game. Have fun!</h3>
-        <button @click.prevent="useStore.logOutUser">Logout</button>
+        <button @click.prevent="logOut">Logout</button>
     </header>
     <TicTacToe />
 </template>
 
 <style scoped>
+button{
+    width: 60px;
+    height: 40px;
+}
 </style>
